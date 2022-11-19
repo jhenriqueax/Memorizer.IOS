967c3ff7996930302680f56ff76e1704706d2452
João Henrique Almeida Xavier <63925427+jhenriqueax@users.noreply.github.com>
2022-11-19 18:24:27 -0300

    start

---
+++ Memorizer/Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by João Henrique Almeida Xavier on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
